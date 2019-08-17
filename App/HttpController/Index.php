b9fec6f9d8ba4f7b6a782782f50df81402cbcfa7
hanyu <you@example.com>
2019-08-17 16:51:26 +0800

    remove code

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -17,23 +17,9 @@ class Index extends Base
     {
         // TODO: Implement index() method.
 
-        //获取配置项
-        //file_put_contents('./aaa.log',Config::getInstance()->getConf('MYSQL.host'));
-
-        /*$this->response()->write('hello world');
-        $conf = new \EasySwoole\Mysqli\Config(\EasySwoole\EasySwoole\Config::getInstance()->getConf('MYSQL'));
-        $db = new \EasySwoole\Mysqli\Mysqli($conf);
-        $data = $db->get('users');*/
-
         //file_put_contents('./aaa.log',json_encode($data));
-        // 异步任务
-        //TaskManager::async(\App\Tasks\TestTask::class);
-        /*$redis = \App\Utility\Pool\RedisPool::defer();
-        $redis->set('name','hanyu');
-        var_dump($redis->get('name'));*/
-        //(new \App\Repositories\UserRepository)->test();
         // 实例化任务模板类 并将数据带进去 可以在任务类$taskData参数拿到数据
-        $this->response()->withHeader('Content-type', 'text/html;charset=utf-8');
+        //$this->response()->withHeader('Content-type', 'text/html;charset=utf-8');
 
         /*$taskClass = new TestTask();
         \EasySwoole\EasySwoole\Swoole\Task\TaskManager::async($taskClass);
@@ -46,23 +32,5 @@ class Index extends Base
         /*Timer::getInstance()->loop(10 * 1000, function () {
             file_put_contents('./aaa.log', microtime(true) . PHP_EOL, FILE_APPEND);
         });*/
-    }
-
-    function test()
-    {
-        $redis = $this->getRedisPool();
-
-        $redis->set('name', 'Rean');
-        $this->response()->withHeader('Content-type', 'text/html;charset=utf-8');
-        $this->response()->write($redis->get('name'));
-
-        //$this->response()->write('this is test123');
-        //return '/test2';//当执行完test方法之后,返回/test2,让框架继续调度/test2方法
-    }
-
-    function test2()
-    {
-        $this->response()->write('this is test2');
-        return true;
     }
 }
